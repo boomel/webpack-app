a792b4134873f2521b6fcacee8ff0f6d356fcd30
boomel <boomel1@gmail.com>
2018-05-16 19:45:57 +0300

    add code during tutorial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import createMenu from './menu';
-var menu = createMenu(['Главная', 'Обо мне','Портфолио'], 'menu');
-document.body.appendChild(menu);
+// import createMenu from './menu';
+// var menu = createMenu(['Главная', 'Портфолио'], 'menu');
+// document.body.appendChild(menu);
+
+import $ from 'jquery';
+
+$('.title').html('Some text 345345 gfdgdfg');
+
+import './style/style.css'
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,69 @@
 const path = require('path');
-const HtmlWebpackPLugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// const HtmlWebpackPLugin = require('html-webpack-plugin');
 
-const PATHS = {
-	source: path.join(__dirname, 'src'),
-	build: path.join(__dirname, 'dist')
-};
+// const PATHS = {
+// 	source: path.join(__dirname, 'src'),
+// 	build: path.join(__dirname, 'dist')
+// };
 
-const config = {
-	entry: {
-		main: PATHS.source + 'index.js'
+// const config = {
+// 	entry: {
+// 		main: PATHS.source + 'index.js'
+// 	},
+// 	output: {
+// 		path: PATHS.build,
+// 		filename: '[name].js'
+// 	}	
+// };
+
+// module.exports = {
+// 	mode: 'development',
+// 	plugins: [
+// 		new HtmlWebpackPLugin({
+// 			title: 'Webpack app'
+// 		})
+// 	]
+// }
+
+let conf = {
+	entry: './src/index.js',
+	output : {
+		path: path.resolve(__dirname, './dist/'),
+		filename: 'main.js',
+		publicPath: 'dist/'
 	},
-	output: {
-		path: PATHS.build,
-		filename: '[name].js'
-	}	
+	devServer : {
+		overlay: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				//exclude: 'node_modules/'
+			},
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: "css-loader"
+				})
+				//use: [
+				//	'style-loader',
+				//	'css-loader'
+				//]
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin("styles.css")
+	]	
 };
 
-module.exports = {
-	mode: 'development',
-	plugins: [
-		new HtmlWebpackPLugin({
-			title: 'Webpack app'
-		})
-	]
-}
+module.exports = (env, options) => {
+	let production = options.mode === 'production';
+	conf.devtool = production ? 'source-map' : 'eval-sourcemap';
 
+	return conf;
+};
